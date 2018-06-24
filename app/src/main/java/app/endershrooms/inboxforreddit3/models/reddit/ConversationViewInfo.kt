ff656cb0ab971d725002f16d4c8a9d0481c3d749
Travis <travisgayledev@gmail.com>
2018-06-23 23:48:24 -0400

     

--- app/src/main/java/app/endershrooms/inboxforreddit3/models/reddit/ConversationViewInfo.kt
+++ app/src/main/java/app/endershrooms/inboxforreddit3/models/reddit/ConversationViewInfo.kt
@@ -1,7 +1,7 @@
 package app.endershrooms.inboxforreddit3.models.reddit
 
 class ConversationViewInfo(message: Message) {
-    val parentName : String = message.parentMessageName
-    val correspondent : String  = message.getCorrespondent()
-    val subject : String = message.subject
+    val parentName = message.parentMessageName
+    val correspondent = message.getCorrespondent()
+    val subject = message.subject
 }
